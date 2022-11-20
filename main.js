40025031ffb2f82c85cde9c842493906e5de46ad
puntoexe77 <dixman.17@gmail.com>
2022-11-20 17:38:03 -0500

    Al index.html se agrego el menu mobile, al archivo style.css sus respectivos estilos y al main.js le dimos dinamica de cerrar y mostrar, ademas de sincronizar su cierre con el menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',showDesktopMenu);
+burguerMenu.addEventListener('click',showMobileMenu);
 
 function showDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive', true);
+}
+function showMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive', true);
 }
