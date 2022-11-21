cd26d14a455593bef1a45989903194f26fa0b29d
puntoexe77 <dixman.17@gmail.com>
2022-11-21 07:26:22 -0500

    Al index.html se agrego el carito de compras, la lista principal,ademas al archivo style.css sus respectivos estilos y al main.js le dimos dinamica de cerrar y mostrar, ademas de sincronizar su cierre con el menu mobile y el desktop menu

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
+//mostrar y ocultar el desktop menu - bloque 1
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+menuEmail.addEventListener('click',showDesktopMenu);
+
+//mostrar y ocultar el mobil menu - bloque 2
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
-menuEmail.addEventListener('click',showDesktopMenu);
 burguerMenu.addEventListener('click',showMobileMenu);
 
+//mostrar y ocultar el product detail- bloque 3 
+const shopCar = document.querySelector('.navbar-shopping-cart');
+const shoppingCartContainer = document.querySelector('.product-detail');
+shopCar.addEventListener('click',showDetailProduct);
+
+// funcion del bloque 1
 function showDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     mobileMenu.classList.toggle('inactive', true);
+    shoppingCartContainer.classList.toggle('inactive',true);
 }
+
+// funcion del bloque 2
 function showMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
     desktopMenu.classList.toggle('inactive', true);
+    mobileMenu.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive',true);
+    
+}
+
+// funcion del bloque 3
+function showDetailProduct(){
+    desktopMenu.classList.toggle('inactive',true);
+    mobileMenu.classList.toggle('inactive',true);
+    shoppingCartContainer.classList.toggle('inactive');
 }
